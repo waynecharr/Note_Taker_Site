c2688f45780f56f6b40db3ec5daf6dfd0a0396a5
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-11-16 21:26:47 -0500

    Added the get route for the api-routes

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - "initial commit" 
 - "began setting up the routes for the api-routes and the html-routes. Started the server.js file" 
 - "Made some additional updates to the server.js file." 
+- "Added the get route for the api-routes" 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,13 @@
-//starter code
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const uniqid = require('uniqid')
+
+router.get('api/notes', (req, res) =>
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+
+
+  
+  module.exports = router;
