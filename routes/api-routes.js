322d9d7efc71883356ade64fb7810397986023ab
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-11-17 20:26:51 -0500

    subtittued the db directory for dbFilepath

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,65 +3,63 @@ const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
+const dbFilePath = path.join(__dirname, 'db', 'db.json');
 
 // GET route reading the db.json file.
 router.get('/api/notes', (req, res) =>
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+  readFromFile(dbFilePath).then((data) => res.json(JSON.parse(data)))
 );
 
 // POST routes for submitting a new note
 router.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
-  
-  
-      const note = {
+
+    const note = {
         title,
         text,
         unique_id: uuidv4(),
-      };
+    };
       
-      // Reads the file
-      fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    // Reads the file
+    fs.readFile(dbFilePath, 'utf-8', (err, data) => {
         if (err) {
-          console.error(err);
-          res.status(500).json({ error: 'Internal Server Error' });
-          return;
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+            return;
         }
 
         let notesArray = [];
 
         // Looks to see if the data is available
         if (data) {
-          try {
-            notesArray = JSON.parse(data);
-          } catch (parseError) {
-            console.error(parseError);
-            res.status(500).json({ error: 'Data is not parsing.' });
-            return;
-          }
+            try {
+                notesArray = JSON.parse(data);
+            } catch (parseError) {
+                console.error(parseError);
+                res.status(500).json({ error: 'Data is not parsing.' });
+                return;
+            }
         }
-    
+
         // Adds a new note to the array
         notesArray.push(note);
-    
+
         // Takes the array and updates the file. 
-        fs.writeFile('./db/db.json', JSON.stringify(notesArray, null, 2), (writeErr) => {
-          if (writeErr) {
-            console.error(writeErr);
-            res.status(500).json({ error: 'Error writing to file' });
-            return;
-          }
-    
-          const response = {
-            status: 'success',
-            body: newNote,
-          };
-    
-          res.json(response);
+        fs.writeFile(dbFilePath, JSON.stringify(notesArray, null, 2), (writeErr) => {
+            if (writeErr) {
+                console.error(writeErr);
+                res.status(500).json({ error: 'Error writing to file' });
+                return;
+            }
+
+            const response = {
+                status: 'success',
+                body: note,
+            };
+
+            res.json(response);
         });
-      });
     });
+});
 
-
-  
 module.exports = router;
